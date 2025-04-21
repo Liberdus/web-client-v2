5f96b089b94016345526f9aa926573e54c69b9c1
dnlbui <dnlbui@proton.me>
2025-04-21 11:16:31 -0500

    Update network configuration to switch from Devnet to Testnet, modifying gateway, monitor, explorer URLs, and websocket settings for local development.

--- network.js
+++ network.js
@@ -1,20 +1,20 @@
 const network = {
   "netid": "fd1b56b08fd1e5035aa19eb631f7f1ad0395175c5d3dfc49411dfa528e6af7c3",
-  "name": "Devnet",
+  "name": "Testnet",
   "gateways": [
     {
-      "protocol": "https",
-      "host": "dev.liberdus.com",
+      "protocol": "http",
+      "host": "localhost",
       "port": 3030    
     },
   ],
   "monitor": {
-    "url": "https://dev.liberdus.com/monitor"
+    "url": "https://test.liberdus.com/monitor"
   },
   "explorer": {
-    "url": "https://dev.liberdus.com/explorer"
+    "url": "https://test.liberdus.com/explorer"
   },
   websocket: {
-    url: "wss://dev.liberdus.com:3031",
+    url: "ws://localhost:3031",
   }
 }
