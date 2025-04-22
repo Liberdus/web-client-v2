fade765e864eebb4ad2ca50b813841fb5f974c1d
dnlbui <dnlbui@proton.me>
2025-04-22 14:13:34 -0500

    Update network configuration to change the name from Testnet to Devnet in network.js, aligning with recent development environment adjustments.

--- network.js
+++ network.js
@@ -1,6 +1,6 @@
 const network = {
   "netid": "fd1b56b08fd1e5035aa19eb631f7f1ad0395175c5d3dfc49411dfa528e6af7c3",
-  "name": "Testnet",
+  "name": "Devnet",
   "gateways": [
     {
       "protocol": "http",
