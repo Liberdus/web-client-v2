74e9c9f0489a3935ede41e191305dd15dae7afe1
dnlbui <dnlbui@proton.me>
2025-05-23 12:36:12 -0500

    Update transaction fee handling in app.js to use a fixed toll value for message sending

--- app.js
+++ app.js
@@ -3392,7 +3392,7 @@ async function handleSendMessage() {
         // can create a function to query the account and get the receivers toll they've set
         // TODO: will need to query network and receiver account where we validate
         // TODO: decided to query everytime we do openChatModal and save as global variable. We don't need to clear it but we can clear it when closing the modal but should get reset when opening the modal again anyway
-        const toll = networkAccountData('current.transactionFee');
+        const toll = 1n * wei;
 
         const chatMessageObj = createChatMessage(currentAddress, payload, toll, keys);
         const txid = await signObj(chatMessageObj, keys)
