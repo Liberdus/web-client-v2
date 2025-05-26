edf5fffbf3866851334eaf050906f3ebfde11fc7
dnlbui <dnlbui@proton.me>
2025-05-26 12:39:34 -0500

    fix: Correct endpoint path for old receipts in checkPendingTransactions function
    - Updated the endpoint path to include a leading slash for consistency with API structure, changing from `collector/api/transaction?appReceiptId=${txid}` to `/collector/api/transaction?appReceiptId=${txid}`.

--- app.js
+++ app.js
@@ -7734,7 +7734,7 @@ async function checkPendingTransactions() {
 
             let endpointPath = `/transaction/${txid}`;
             if (submittedts < twentySecondsAgo || submittedts < thirtySecondsAgo) {
-                endpointPath = `collector/api/transaction?appReceiptId=${txid}`;
+                endpointPath = `/collector/api/transaction?appReceiptId=${txid}`;
             }
             //console.log(`DEBUG: txid ${txid} endpointPath: ${endpointPath}`);
             const res = await queryNetwork(endpointPath);
