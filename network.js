2233014799a073bf6cbc52a054d8e7e5d97b5ece
dnlbui <dnlbui@proton.me>
2025-05-25 19:04:28 -0500

    Update .prettierignore and network.js for improved configuration and functionality
    - Added external libraries to .prettierignore to streamline formatting processes.
    - Updated network.js to change gateway protocol to HTTPS and adjusted URLs for monitoring and exploring to point to the development environment, enhancing security and accuracy.

--- network.js
+++ network.js
@@ -1,20 +1,21 @@
+/* eslint-disable no-unused-vars */
 const network = {
-  "netid": "fd1b56b08fd1e5035aa19eb631f7f1ad0395175c5d3dfc49411dfa528e6af7c3",
-  "name": "Devnet",
-  "gateways": [
+  netid: 'fd1b56b08fd1e5035aa19eb631f7f1ad0395175c5d3dfc49411dfa528e6af7c3',
+  name: 'Devnet',
+  gateways: [
     {
-      "protocol": "http",
-      "host": "localhost",
-      "port": 3030    
+      protocol: 'https',
+      host: 'dev.liberdus.com',
+      port: 3030,
     },
   ],
-  "monitor": {
-    "url": "https://test.liberdus.com/monitor"
+  monitor: {
+    url: 'https://dev.liberdus.com/monitor',
   },
-  "explorer": {
-    "url": "https://test.liberdus.com/explorer"
+  explorer: {
+    url: 'https://dev.liberdus.com/explorer',
   },
   websocket: {
-    url: "ws://localhost:3031",
-  }
-}
+    url: 'wss://dev.liberdus.com:3031',
+  },
+};
