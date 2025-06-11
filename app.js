d342104e487accea1e256e02254104e15c0e8e7b
osyed <osyed1@gmail.com>
2025-06-11 10:56:52 -0500

    fix update the send asset form info message when user enters memo

--- app.js
+++ app.js
@@ -8590,6 +8590,7 @@ class SendAssetFormModal {
     this.memoInput.value = '';
     this.retryTxIdInput.value = '';
     this.tollMemoSpan.textContent = '';
+    this.foundAddressObject.address = null;
 
     this.usernameAvailable.style.display = 'none';
     this.submitButton.disabled = true;
@@ -8643,6 +8644,7 @@ class SendAssetFormModal {
     }
 
     this.clearFormInfo()
+    this.foundAddressObject.address = null;
 
     // Check if username is too short
     if (username.length < 3) {
