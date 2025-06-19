e2a2ae86075b5b3f92f9f3bb2250f5a337904602
dnlbui <dnlbui@proton.me>
2025-06-19 10:44:57 -0500

    Enhance input validation in SendAssetFormModal and validateBalance function
    - Updated the validateBalance function to handle zero amounts, logging a warning and returning false if the amount is zero.
    - Improved input handling in SendAssetFormModal by trimming whitespace from the amount input and adding validation for empty amounts, disabling the submit button and clearing warnings accordingly.
    - Enhanced user feedback with console warnings for empty or invalid amounts, improving overall user experience.

--- app.js
+++ app.js
@@ -2321,7 +2321,10 @@ function fillStakeAddressFromQR(data) {
  */
 async function validateBalance(amount, assetIndex = 0, balanceWarning = null) {
   if (balanceWarning) balanceWarning.style.display = 'none';
-  if (amount < 0n) {
+  if (amount == 0n) {
+    console.warn('[validateBalance] amount is zero');
+    return false;
+  } else if (amount < 0n) {
     console.warn('[validateBalance] amount is negative');
     if (balanceWarning) balanceWarning.style.display = 'block';
     balanceWarning.textContent = 'Amount cannot be negative';
@@ -9015,13 +9018,21 @@ class SendAssetFormModal {
     const isAddressConsideredValid =
       this.usernameAvailable.style.display === 'inline' && this.usernameAvailable.textContent === 'found';
 
-    const amount = this.amountInput.value;
+    const amount = this.amountInput.value.trim();
+
+    if (amount == '') {
+      console.warn('[refreshSendButtonDisabledState] amount is empty');
+      this.balanceWarning.textContent = '';
+      this.balanceWarning.style.display = 'none';
+      this.submitButton.disabled = true;
+      return;
+    }
+
     const assetIndex = this.assetSelectDropdown.value;
 
     // Check if amount is in USD and convert to LIB for validation
     const isUSD = this.balanceSymbol.textContent === 'USD';
     let amountForValidation = amount;
-
     if (isUSD && amount) {
       await getNetworkParams();
       const scalabilityFactor = parameters.current.stabilityScaleMul / parameters.current.stabilityScaleDiv;
