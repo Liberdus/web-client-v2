edc9ffc09be7949dc7200c3c310d5422520bceaf
dnlbui <dnlbui@proton.me>
2025-07-11 13:14:51 -0500

    Add netid extraction functionality to RestoreAccountModal and update network configurations
    - Implemented file input listeners in RestoreAccountModal to extract netids from a selected file and populate dropdowns.
    - Added logic to handle encrypted files and parse netids from localStorage keys.
    - Updated network.js, network.js_loc, and network.js_web to change gateway URLs to secure connections and added an old netid for reference.

--- network.js
+++ network.js
@@ -3,11 +3,12 @@ const network = {
   "name": "Devnet",
   "gateways": [
     {
-      "web": "http://localhost:3030",
-      "ws": "ws://localhost:3031"
+      "web": "https://dev.liberdus.com:3030",
+      "ws": "wss://dev.liberdus.com:3031"
     },
   ],
   "netids": [
     "627510f0cb0bf5e82b0cc8bace10a1a3649c74d9b733af9a32e26d495e5799fe", // current
+    "fd1b56b08fd1e5035aa19eb631f7f1ad0395175c5d3dfc49411dfa528e6af7c3", // old
   ],
 }
