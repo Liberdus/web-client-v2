0e36878712e3d54391cb3ada386e161adb36ee82
Chris Freels <cafreels@gmail.com>
2025-09-09 15:21:58 -0500

    Fix: send read transaction only when online in ChatModal close method

--- app.js
+++ app.js
@@ -7681,14 +7681,16 @@ class ChatModal {
    * @returns {void}
    */
   close() {
-    const needsToSendReadTx = this.needsToSend();
-    console.log(`[close] needsToSendReadTx: ${needsToSendReadTx}`);
-    // if newestRecevied message does not have an amount property and user has not responded, then send a read transaction
-    if (needsToSendReadTx) {
-      this.sendReadTransaction(this.address);
+    if (isOnline) {
+      const needsToSendReadTx = this.needsToSend();
+      console.log(`[close] needsToSendReadTx: ${needsToSendReadTx}`);
+      // if newestRecevied message does not have an amount property and user has not responded, then send a read transaction
+      if (needsToSendReadTx) {
+        this.sendReadTransaction(this.address);
+      }
+      
+      this.sendReclaimTollTransaction(this.address);
     }
-
-    this.sendReclaimTollTransaction(this.address);
 
     // Save any unsaved draft before closing
     this.debouncedSaveDraft(this.messageInput.value);
