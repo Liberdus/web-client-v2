cfb66b9fda9eb06ee04a31970045516c957eff1f
dnlbui <dnlbui@proton.me>
2025-10-03 11:55:40 -0500

    Fix fullscreen toggle logic in app.js to correctly enter fullscreen mode when the keyboard is detected, enhancing mobile user experience.

--- app.js
+++ app.js
@@ -17074,13 +17074,13 @@ function handleKeyboardFullscreenToggle() {
   // If viewport height decreased significantly, keyboard is likely open
   if (heightDifference > 150) { // 150px threshold for keyboard detection
     if (isInFullscreen()) {
-      enterFullscreen();
-    }
-  } else if (heightDifference < 50) { // If height increased or stayed similar, keyboard is likely closed
-    if (!isInFullscreen()) {
       setTimeout(() => {
         exitFullscreen();
       }, 300);
+    }
+  } else if (heightDifference < 50) { // If height increased or stayed similar, keyboard is likely closed
+    if (!isInFullscreen()) {
+      enterFullscreen();
     }
   }
 }
