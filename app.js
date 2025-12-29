b5cc21497e716d5b8c086b01a46d11166f6cc82e
dnlbui <dnlbui@proton.me>
2025-12-29 13:22:04 -0600

    Enhance connectivity-dependent element marking in app.js
    - Added new selectors for wallet, send asset, profile, and stake modal related elements to improve user interaction and ensure proper functionality when connectivity changes.
    - Included buttons for buying, selling, and faucet bridge actions, as well as new fields for username and private key input.

--- app.js
+++ app.js
@@ -6377,10 +6377,14 @@ function markConnectivityDependentElements() {
 
     // Wallet related
     '#refreshBalance',
+    '#openBuyButton',
+    '#openSellButton',
+    '#openFaucetBridgeButton',
     '#sendForm button[type="submit"]',
 
     // Send asset related
     '#sendAssetForm button[type="submit"]',
+    '#sendToAddress',
     '#toggleBalance',
 
     // Add friend related
@@ -6395,10 +6399,14 @@ function markConnectivityDependentElements() {
     // Profile related
     '#createAccountForm button[type="submit"]',
     '#importForm button[type="submit"]',
+    '#newUsername',
+    '#newPrivateKey',
+    '#migrateAccountsButton',
 
     // stakeModal
     '#submitStake',
     '#faucetButton',
+    '#stakeNodeAddress',
 
     // tollModal
     '#saveNewTollButton',
